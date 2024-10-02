6f6b6af9636610a4855fe34a7228930e53981348
SceneryMC <1330827323@qq.com>
2024-10-02 15:09:47 +0800

    更新curl下载paper的脚本

--- misc/download_newest_paper.py
+++ misc/download_newest_paper.py
@@ -1,14 +1,14 @@
 import os
 import json
-from secret import misc_minecraft_server_path
+from secret import misc_minecraft_server_path, proxies
 
 minecraft_server_path = misc_minecraft_server_path
-with os.popen("curl -X 'GET' \
+with os.popen(f"curl --proxy {proxies['http']} -X 'GET' \
   'https://api.papermc.io/v2/projects/paper' \
   -H 'accept: application/json'") as f:
     result = json.load(f)
 latest_version = result['versions'][-1]
-with os.popen(f"curl -X 'GET' \
+with os.popen(f"curl --proxy {proxies['http']} -X 'GET' \
   'https://api.papermc.io/v2/projects/paper/versions/{latest_version}/builds' \
   -H 'accept: application/json'") as f:
     result = json.load(f)
@@ -16,10 +16,10 @@ latest_build_json = result['builds'][-1]
 num, name = latest_build_json['build'], latest_build_json['downloads']['application']['name']
 old_version = [s for s in os.listdir(minecraft_server_path) if 'paper-' in s][0]
 if old_version != name:
-    os.remove(f"{minecraft_server_path}/{old_version}")
-    os.system(f"curl -X 'GET' \
+    os.system(f"curl --proxy {proxies['http']} -X 'GET' \
       'https://api.papermc.io/v2/projects/paper/versions/{latest_version}/builds/{num}/downloads/{name}' \
       -H 'accept: application/json' \
       --output '{minecraft_server_path}/paper-{latest_version}-{num}.jar'")
+    os.remove(f"{minecraft_server_path}/{old_version}")
     os.system(f"dolphin {minecraft_server_path} & disown")
     exit(0)
